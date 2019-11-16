95ce5c7a72abfe94dff8f54f6210a9d475d456f6
Quan Nguyen <nguyen.hong.quan282@gmail.com>
2019-11-16 22:18:04 +0700

    Create CustomGlobalExceptionHandler.java

---
+++ src/main/java/com/hvcg/api/task_management/CustomGlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.hvcg.api.task_management;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	
+    @ExceptionHandler
+    ResponseEntity handleConstrantViolation(DataIntegrityViolationException e) {
+    	
+    	return new ResponseEntity("Duplicate Entry ", new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+    
+    @ExceptionHandler
+    ResponseEntity handleNoObjectFound(NoSuchElementException e) {
+    	
+    	return new ResponseEntity("No Object can be found ", new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+    
+
+	
+}

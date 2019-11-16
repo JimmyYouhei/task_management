3054ea436e9d33b20d0b3d0de3ee5400ce6053a0
Quan Nguyen <nguyen.hong.quan282@gmail.com>
2019-11-16 17:45:16 +0700

    clean

--- src/main/java/com/hocvoichuyengia/spring/task_management/dao/SubtaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hocvoichuyengia.spring.task_management.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hocvoichuyengia.spring.task_management.entity.Subtask;
-
-public interface SubtaskRepository extends JpaRepository<Subtask, Integer> {
-
-}
